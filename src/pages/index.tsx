74e35bcb74080d2d9abdf4b98902c1baac798fba
João Batista da Silva <joaojbs199@gmail.com>
2022-05-07 17:57:45 -0300

    Personalização.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,22 @@
 import styles from '@styles/Index.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTools } from "@fortawesome/free-solid-svg-icons";
+import Image from "next/image";
+import marca from '@public/marca.png';
 
 export default function Home() {
     return (
-        <div>
-            <h1>Site em manutenção</h1>
+        <div className={styles.container}>
+            <div className={styles.maintenanceInfo}>
+                <div className={styles.divLogo}>
+                    <Image src={marca} alt="logo" width={"200px"} height={"80px"} />
+                </div>
+                <h1 className={styles.title}>Site em manutenção</h1>
+                <h2 className={styles.subtitle}>Validação de sistema.</h2>
+                <div>
+                    <FontAwesomeIcon className={styles.icon} beatFade icon={faTools} />
+                </div>
+            </div>
         </div>
     )
 }
