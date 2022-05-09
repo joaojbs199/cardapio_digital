058b100aa4b7147e2142d7750049e03d1e813ae5
João Batista da Silva <joaojbs199@gmail.com>
2022-05-09 19:22:47 -0300

    Refatoração da função de download do Qrcode. Separação das funções de shareAccess do arquivo do componente. (middleware).

--- src/components/shareAccessMenu.tsx
+++ src/components/shareAccessMenu.tsx
@@ -1,14 +1,15 @@
 import styles from '@styles/ShareAccessMenu.module.css';
-import { useContext } from 'react';
+import { useContext, useRef } from 'react';
 import { Controllers } from '@contexts/ControllersContext';
+import { exportElementAsImage, shareLink, openCardapio } from "@services/shareAccessMiddleware";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark, faArrowUpRightFromSquare, faDownload, faSpinner, faShare } from "@fortawesome/free-solid-svg-icons";
-import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 import Image from 'next/image';
 
 export default function ShareAccessMenu(props) {
 
     const { panelsState, setPanelsState } = useContext(Controllers);
+    const qrCodeCardDownload = useRef();
     const { qrCode, link } = props;
 
     const establishmentName = props.establishmentName?.normalize('NFD')
@@ -16,39 +17,6 @@ export default function ShareAccessMenu(props) {
         .replace(/\s+$/g, '')
         .replace(/ /g, '-');    
 
-
-    /* const downloadQrCode = () => {
-        const downloadTag = document.createElement('a');
-        downloadTag.href = qrCode;
-        downloadTag.setAttribute('download', `qrcode.png`);
-        document.body.appendChild(downloadTag);
-        downloadTag.click();
-        document.body.removeChild(downloadTag);
-    } */
-
-    const shareLink = () => {
-        
-        if(navigator.share) {
-
-            navigator.share({
-                title: "Easy menu - Cardápio digital",
-                text: "Este é o link do nosso cardápio! Acesse e confira nossas opções.",
-                url: link
-            });
-        } else {
-            navigator.clipboard.writeText(link).then(() => alert("Copiado para a área de transferência"));
-        }
-    }
-
-    const openCardapio = () => {
-        const cardapioUrl = link;
-        const openTag = document.createElement('a');
-        openTag.href = cardapioUrl;
-        openTag.target = '_blank';
-        document.body.appendChild(openTag);
-        openTag.click();
-        document.body.removeChild(openTag);
-    }
 
     return (
         <>
@@ -64,7 +32,7 @@ export default function ShareAccessMenu(props) {
                 <div className={styles.shareAccessDiv}>
 
                     <div className={styles.divQrCodeOuter}>
-                        <div className={styles.qrCodeCard}>
+                        <div ref={qrCodeCardDownload} className={styles.qrCodeCard}>
                             <div className={styles.qrCodeFrame}>
                             { !qrCode && <FontAwesomeIcon className={styles.qrCodeLoader} pulse icon={faSpinner} /> }
                                 {qrCode && <Image 
@@ -79,21 +47,21 @@ export default function ShareAccessMenu(props) {
 
                         <div className={styles.buttonsToShareDiv}>
 
-                            <button disabled style={{backgroundColor: '#DDD'}} onClick={() => {
-                                //downloadQrCode();
+                            <button onClick={() => {
+                                exportElementAsImage(qrCodeCardDownload.current, `qrcode_${establishmentName}`)
                             }} className={styles.shareButtons}>
                                 <FontAwesomeIcon className={styles.shareButtonsIcon} icon={faDownload} />
                             </button>
 
                             <button onClick={() => {
-                                shareLink();
+                                shareLink(link);
                                 setPanelsState({name: 'emptyPresentation'});
                             }} className={styles.shareButtons}>
                                 <FontAwesomeIcon className={styles.shareButtonsIcon} icon={faShare} />
                             </button>
 
                             <button onClick={() => {
-                                openCardapio();
+                                openCardapio(link);
                                 setPanelsState({name: 'emptyPresentation'});
                             }} className={styles.shareButtons}>
                                 <FontAwesomeIcon className={styles.shareButtonsIcon} icon={faArrowUpRightFromSquare} />
