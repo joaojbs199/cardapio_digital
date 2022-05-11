885f2e89895d2d7709e9c563242789e5d34bf080
João Batista da Silva <joaojbs199@gmail.com>
2022-05-11 17:59:26 -0300

    Remoção de redirecionamento automático da página inicial. Estilização de botão de acesso à conta (login ).

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,48 +1,14 @@
 import Router from 'next/router';
 import { serverBasicRequest } from '@services/serverRequests';
 import { parseCookies } from "nookies";
+import styles from '@styles/Main.module.css';
 
 export default function Home() {
     return (
-        <div style={{width: '100%', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-            <button style={{padding: '10px'}} onClick={() => Router.push('/login')}>
-                Login
+        <div className={styles.container}>
+            <button className={styles.loginButton} onClick={() => Router.push('/login')}>
+                LOGIN
             </button>
         </div>
     )
 }
-
-export const getServerSideProps = async (context) => {
-
-    const serverSideRequests = serverBasicRequest(context); // Makes request sending context
-  
-    const {['__UEMAT']: token } = parseCookies(context);
-    
-    if(token) {
-      const user = await serverSideRequests.post('/api/auth/reloadUser', {token})
-      .then(response => {
-        const user = response.data.user;
-        return user;
-      });
-      console.log(user);
-      if(user) {
-        const routeName = (user.establishmentName)
-        .normalize('NFD')
-        .replace(/[\u0300-\u036f]/g,'')
-        .replace(/ /g, '-');
-    
-        return{
-          redirect:{
-            destination: `/userPanel/${routeName}`,
-            permanent: false
-          }
-        }
-      }
-    }
-    
-    return{
-      props: {
-  
-      }
-    }
-  }
